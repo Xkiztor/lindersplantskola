de0e31ec484f66cbd0b66d4cd0a8b28503061475
Xki <ugo.linder@gmail.com>
2023-02-27 15:32:42 +0100

    storyblock

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    'nuxt-icon',
-    '@vueuse/nuxt',
+    // 'nuxt-icon',
+    // '@vueuse/nuxt',
     ["@storyblok/nuxt", { accessToken: "vQAge7njq09k7mMMgmWRLgtt" }]
   ]
 })
