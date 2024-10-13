638c58806a34a07098034c8ca74830ca278d276b
Xki <ugo.linder@gmail.com>
2024-10-13 21:08:16 +0200

    hero change

---
+++ components/RichText.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['element', 'content']);
+
+const processedContent = computed(() => {
+  console.log(props.content);
+  // console.log(JSON.parse(props.content));
+  if (props.content) {
+    return props.content;
+  } else {
+    return [''];
+  }
+});
+</script>
+
+<template>
+  <component v-for="text in processedContent" :is="element">
+    {{ text }}
+  </component>
+</template>
+
+<style></style>
--- pages/admin/index-change.vue
+++ pages/admin/index-change.vue
@@ -1,24 +1,6 @@
 <script setup>
 const client = useSupabaseClient();
 
-const { data: images } = await useAsyncData(
-  'lindersplantskola-config',
-  async () => {
-    const { data, error } = await client
-      .from('lindersplantskola-config')
-      .select()
-      .eq('inställning', 'heroBilder')
-      .single();
-    if (error) console.error(error);
-    if (data) {
-      console.log(data);
-      heroBilderInput.value = data.värde;
-    }
-
-    return data.värde;
-  }
-);
-
 // ------------ heroBilder ----------------
 const heroBilderSaved = ref(false);
 
@@ -47,27 +29,109 @@ const heroBilderUpdate = async () => {
 
 // ------------ heroText ----------------
 const heroTextSaved = ref(false);
+
+const heroTextInput = ref();
+
+watch(heroTextInput, () => {
+  heroBilderSaved.value = false;
+});
+
+const heroTextUpdate = async () => {
+  console.log('updating');
+
+  const { data, error } = await client
+    .from('lindersplantskola-config')
+    // .update({ värde: `test` })
+    .update({ värde: `${heroTextInput.value}` })
+    .eq('inställning', 'heroText')
+    .select();
+  if (error) console.error(error);
+  if (data) {
+    console.log(data);
+    heroTextSaved.value = true;
+  }
+};
+
+// --------------------------
+
+const { data: images } = await useAsyncData(
+  'lindersplantskola-config',
+  async () => {
+    const { data, error } = await client
+      .from('lindersplantskola-config')
+      .select();
+    // .single();
+    // .eq('inställning', 'heroBilder')
+    if (error) console.error(error);
+    if (data) {
+      console.log(data);
+
+      console.log(data.filter((e) => e.inställning === 'heroBilder')[0].värde);
+
+      heroBilderInput.value = data.filter(
+        (e) => e.inställning === 'heroBilder'
+      )[0].värde;
+
+      heroTextInput.value = data.filter(
+        (e) => e.inställning === 'heroText'
+      )[0].värde;
+    }
+
+    return data.värde;
+  }
+);
 </script>
 
 <template>
   <div>
-    <div>
+    <div class="inställning">
       <h2>Bilder i bildkarusselen</h2>
       <p>Format: ["https", "https"]</p>
       <textarea type="text" v-model="heroBilderInput" />
       {{ images }}
-      <button @click="heroBilderUpdate()">Spara</button>
-      <p class="saved" v-if="heroBilderSaved">Sparad!</p>
+      <div class="save">
+        <button @click="heroBilderUpdate()">Spara</button>
+        <p class="saved" v-if="heroBilderSaved">Sparad!</p>
+      </div>
     </div>
-    <div>
+    <div class="inställning">
       <h2>Beskrivning brevid bildkarussel</h2>
       <p>Format:</p>
-      <textarea type="text" v-model="heroBilderInput" />
+      <textarea type="text" v-model="heroTextInput" />
       {{ images }}
-      <button @click="heroBilderUpdate()">Spara</button>
-      <p class="saved" v-if="heroBilderSaved">Sparad!</p>
+      <div class="save">
+        <button @click="heroTextUpdate()">Spara</button>
+        <p class="saved" v-if="heroTextSaved">Sparad!</p>
+      </div>
+    </div>
+    <div class="inställning">
+      <h2>Öppetider</h2>
+      <p>Format:</p>
+      <textarea type="text" v-model="heroTextInput" />
+      {{ images }}
+      <div class="save">
+        <button @click="heroTextUpdate()">Spara</button>
+        <p class="saved" v-if="heroTextSaved">Sparad!</p>
+      </div>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.inställning {
+  margin-bottom: 1rem;
+}
+
+.inställning textarea {
+  width: 100%;
+  resize: vertical;
+  min-height: 5rem;
+}
+
+.save {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
